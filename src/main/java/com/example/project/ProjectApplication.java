d62a2bda480ca331369552c4da44c4caa91fb1f3
vilmatee <vilzqu@gmail.com>
2020-04-23 14:05:05 +0300

    fixing heroku

--- src/main/java/com/example/project/ProjectApplication.java
+++ src/main/java/com/example/project/ProjectApplication.java
@@ -25,33 +25,33 @@ public class ProjectApplication {
 	public CommandLineRunner demo(AstiaRepository repository, CategoryRepository crepository, KayttajaRepository urepository) {
 		return (args) -> {
 			//Luodaan kategoriat astioille
-			crepository.save(new Category("Mukit"));
-			crepository.save(new Category("Lautaset"));
-			crepository.save(new Category("Kulhot"));
-			crepository.save(new Category("Muut"));
+			//crepository.save(new Category("Mukit"));
+			//crepository.save(new Category("Lautaset"));
+			//crepository.save(new Category("Kulhot"));
+			//crepository.save(new Category("Muut"));
 			
-			//crepository.deleteAll();
+		//	crepository.deleteAll();
 			
 			//Luodaan astioita
-			Astia a1 = new Astia("Räsymatto", "Mustavalkoinen","4 dl", 24.00, 2018, crepository.findByName("Mukit").get(0));
-			Astia a2 = new Astia("Siirtolapuutarha", "Mustavalkoinen","4 dl", 24.00, 2018, crepository.findByName("Mukit").get(0));
-			Astia a3 = new Astia("Kestit", "Vaaleanpunainen","2,5 dl", 17.95, 2019, crepository.findByName("Lautaset").get(0));
+			//Astia a1 = new Astia("Räsymatto", "Mustavalkoinen","4 dl", 24.00, 2018, crepository.findByName("Mukit").get(0));
+			//Astia a2 = new Astia("Siirtolapuutarha", "Mustavalkoinen","4 dl", 24.00, 2018, crepository.findByName("Mukit").get(0));
+			//Astia a3 = new Astia("Kestit", "Vaaleanpunainen","2,5 dl", 17.95, 2019, crepository.findByName("Lautaset").get(0));
 			
 			//Tallennetaan astiat
-			repository.save(a1);
-			repository.save(a2);
-			repository.save(a3);
+			//repository.save(a1);
+			//repository.save(a2);
+			//repository.save(a3);
 			
-			//repository.deleteAll();
+		//	repository.deleteAll();
 			
 		//Luodaan käyttäjiä, määritetään niiden salasanat (hash), ja määritetään niiden oikeudet
-		Kayttaja user1 = new Kayttaja("user", "$2a$10$hbzTguGsymIbJ79hnnSJCuOVobdH6pNvem7rlAo008R1Bhh3GuIVS", "USER");
-		Kayttaja user2 = new Kayttaja("admin", "$2a$10$0QVwv4j29k2wnIeXyAoyx.dn769J8yJJjsI2C7joZ3qff/14O9o2e", "ADMIN");
-		Kayttaja user3 = new Kayttaja("vilma", "$2a$10$PMUZ3BOOw1wh3AKCaghdpu7jrDg.9fsrel4Z.ZCaKL/9TydPB/QX6", "ADMIN");
+		//Kayttaja user1 = new Kayttaja("user", "$2a$10$hbzTguGsymIbJ79hnnSJCuOVobdH6pNvem7rlAo008R1Bhh3GuIVS", "USER");
+		//Kayttaja user2 = new Kayttaja("admin", "$2a$10$0QVwv4j29k2wnIeXyAoyx.dn769J8yJJjsI2C7joZ3qff/14O9o2e", "ADMIN");
+		//Kayttaja user3 = new Kayttaja("vilma", "$2a$10$PMUZ3BOOw1wh3AKCaghdpu7jrDg.9fsrel4Z.ZCaKL/9TydPB/QX6", "ADMIN");
 		
-		urepository.save(user1);
-		urepository.save(user2);
-		urepository.save(user3);
+		//urepository.save(user1);
+		//urepository.save(user2);
+		//urepository.save(user3);
 		
 		//urepository.deleteAll();
 		
